aeea895e7359dc1134adeb92f28d14e1680702ca
Natan Lucena <zerogamerptbr@gmail.com>
2025-09-21 16:23:22 -0300

    feat: update .gitignore, package.json, and add vercel.json for deployment configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express on Vercel!" });
 });
+
+export default app;
